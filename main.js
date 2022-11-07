604f691407ccc922f9675cd19161e8a1a991e016
kw2x <bhen2014@gmail.com>
2022-11-07 18:29:31 -0500

    made changes to HTML

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-const clear = document.getElementsByClassName("clr-btn");
-clear.addEventListener("click", () => {
-  const display = document.getElementsById("display").innerHTML = "";
-  return display;
-})
+
